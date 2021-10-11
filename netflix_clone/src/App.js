c076492bdb364187ca8e20ec9865d011a4ff6e29
KayodeOlusi <Kayodeolusi@gmail.com>
2021-10-11 19:21:00 -0400

    set up the configuration

---
+++ netflix_clone/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-gray-500 text-green-500">
+        Hi
+      </div>
+    </div>
+  );
+}
+
+export default App;
